cb12a9ca2b45ed07470e310304a4860f18e46361
EvgeniiaSiitari1 <ev.siitari@gmail.com>
2020-05-08 14:28:01 +0300

    Test commit changed text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
        <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" >
-            CarShop
+            CarShop !!!!!!
           </Typography>
         </Toolbar>
       </AppBar>
